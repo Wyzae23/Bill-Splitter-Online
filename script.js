e6d4da62f7376e1be8c783073b64c7ebe474a5bb
Harold <haroldmo23@gmail.com>
2022-04-29 01:50:27 -0700

    updated

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-let numPeople = document.querySelector('numPeopleInput');
 
-function textBox(numPeople){
-    numPeople = numPeople*1; // Convert to int
-    if( numPeople !== numPeople ) throw 'Invalid argument'; // Check NaN
-    var container = document.getElementsByClassName('left-side'); //Cache container.
-
-    for(var i = 0; i <= numPeople; i++){
-        var tb = document.createElement('input');
-        tb.type = 'text';
-        tb.id = 'textBox_' + i; // Set id based on "i" value
-        container.appendChild(tb); 
-    }
+function getNumber()
+{
+        var num = document.getElementById("numPeopleInput").value;
+        for(let i = 0; i < num; i++)
+        {
+            var location = document.getElementById("purchase");
+            var input = document.createElement("input");
+            input.setAttribute("id", ""+i);
+            location.appendChild(input);
+        }
 }
